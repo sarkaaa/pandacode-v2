0979b0760f585ad342dea9846e7d763df5f4704d
Sarka Chwastkova <sarka.chwastkova@kiwi.com>
2025-06-28 16:30:43 +0200

    feat: initial setup

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+// Example `tailwind.config.js` file
+const exports = {
+  theme: {
+    extend: {
+      background: {
+        'primary': '#FFFFFF',
+      },
+      colors: {
+        'green-gradient': '#A4E9A9',
+        'blue-gradient': '#BDE4FB',
+        'red-gradient': '#FFB8B9',
+        'purple-gradient': '#DEBDFF',
+      },
+      border: {
+        1: '1px',
+      },
+      // spacing: {
+      //   20: '5rem',
+      // },
+    },
+  },
+}
+
+export default exports;
