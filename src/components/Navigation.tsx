26ba0b749d2ffaa9b15ceeec832e0248585c1406
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-08-30 00:01:04 +0200

    feat: add accessibility reduce motion

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -84,26 +84,26 @@ export default function Navigation() {
   return (
     <nav
       className={clsx(
-        "bg-gray-100/25 dark:bg-black/25 py-3 px-10 rounded-md mx-auto backdrop-blur-xs sm:fixed z-20 transition-all",
-        isScrolled ? "mt-5" : "mt-10",
+        "bg-gray-100/25 dark:bg-black/25 py-3 px-6 md:px-10 rounded-md mx-auto backdrop-blur-xs sm:fixed z-20 motion-safe:transition-all",
+        isScrolled ? "mt-6 md:mt-5" : "mt-6 md:mt-10",
       )}
       >
       <ul className="flex flex-wrap gap-5 justify-start items-center sm:justify-center">
         {PAGES.map(({ title, href }) => (
           <li key={href} className="relative">
             <a 
-              className="peer z-10 p-0.5 border-0 transition-all ease-in-out duration-15 text-black dark:text-light relative decoration-0 no-underline focus:outline-2 focus:outline-red-400 dark:focus:outline-red-300 focus:outline-offset-4 active:bg-gray-100 dark:active:bg-black/25 active:border-b-red-400 dark:active:border-b-red-300 active:font-semibold" 
+              className="peer z-10 p-0.5 border-0 motion-safe:transition-all motion-safe:ease-in-out motion-safe:duration-15 text-black dark:text-light relative decoration-0 no-underline focus:outline-2 focus:outline-red-400 dark:focus:outline-red-300 focus:outline-offset-4 active:bg-gray-100 dark:active:bg-black/25 active:border-b-red-400 dark:active:border-b-red-300 active:font-semibold" 
               href={href}
             >
               {title}
             </a>
-            <span className="bottom-0 xs:bottom-2 absolute z-0 block h-[6px] w-full transition-all duration-300 ease-in-out peer-focus:w-full peer-focus:h-full peer-hover:h-full peer-hover:w-full bg-linear-90 from-red-gradient to-purple-gradient dark:from-dark-blue-gradient dark:to-dark-purple-gradient"></span>
+            <span className="bottom-0 xs:bottom-2 absolute z-0 block h-[6px] w-full motion-safe:transition-all motion-safe:duration-300 motion-safe:ease-in-out peer-focus:w-full peer-focus:h-full peer-hover:h-full peer-hover:w-full bg-linear-90 from-red-gradient to-purple-gradient dark:from-dark-blue-gradient dark:to-dark-purple-gradient"></span>
           </li>
         ))}
         <li>
           <button 
             onClick={handleModeSwitch}
-            className="cursor-pointer p-1 rounded-md bg-linear-90 hover:from-red-gradient hover:to-purple-gradient dark:hover:from-dark-blue-gradient dark:hover:to-dark-purple-gradient transition-colors dark:text-light"
+            className="cursor-pointer p-1 rounded-md bg-linear-90 hover:from-red-gradient hover:to-purple-gradient dark:hover:from-dark-blue-gradient dark:hover:to-dark-purple-gradient motion-safe:transition-colors dark:text-light"
             aria-label="Toggle dark mode"
             id="theme-selector"
           >
